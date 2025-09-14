9219450d37a360c70a36c32b010c18e11e6351d9
XZKpo666 <hd930526@gmail.com>
2025-09-14 20:09:23 +0800

    BGM & SoundEffects code adjust finnish

--- Assets/_Script/Sound/AudioManager.cs
+++ Assets/_Script/Sound/AudioManager.cs
@@ -29,6 +29,7 @@ public class AudioManager : MonoBehaviour, IGameService
         audioSource.Play();   
         StartCoroutine(DestroyAfterSoundEffect(audioSource));
     }
+
     private void IsSoundEffectExist(SoundEffects soundEffect)
     {
         if (soundEffect == null)
@@ -51,33 +52,8 @@ public class AudioManager : MonoBehaviour, IGameService
         audioSource.volume = UnityEngine.Random.Range(soundEffect.MinVolume, soundEffect.MaxVolume);
         audioSource.pitch = UnityEngine.Random.Range(soundEffect.MinPitch, soundEffect.MaxPitch);     
     }
-
-    public void PlayBackgroundMusic(string id)
-    {
-        AudioSource audioSource = Instantiate(_audioPrefab);
-        BackgroundMusic backgroundMusic = Array.Find(_backgroundMusic, sound => sound.Id == id);
-        audioSource.clip = backgroundMusic.AudioCilp;
-        audioSource.volume = backgroundMusic.Volume;
-        audioSource.pitch = backgroundMusic.Pitch;
-        audioSource.loop = backgroundMusic.Loop;
-        
-        if (backgroundMusic == null)
-        {
-            Debug.LogWarning("Background Music: " + name + " not found!");
-            return;
-        }
-
-        if (_oldAudioSource != null)
-        {
-            StartCoroutine(FadeOutMusicAndDestroy(_oldAudioSource));
-        }
-        
-        StartCoroutine(FadeInMusic(audioSource));
-        
-        _oldAudioSource = audioSource; 
-    }
-
-    private IEnumerator DestroyAfterSoundEffect(AudioSource audioSource)
+    
+        private IEnumerator DestroyAfterSoundEffect(AudioSource audioSource)
     {
         while (audioSource.isPlaying)
         {
@@ -85,20 +61,57 @@ public class AudioManager : MonoBehaviour, IGameService
         }
         Destroy(audioSource);
     }
+
+    public void PlayBackgroundMusic(string id)
+    {
+        BackgroundMusic backgroundMusic = Array.Find(_backgroundMusic, sound => sound.Id == id);
+        AudioSource audioSource = Instantiate(_audioPrefab);
+
+        IsBackgroundMusicExist(backgroundMusic);
+        IsOldBackgroundMusic(_oldAudioSource);
+        SetBackgroundMusic(audioSource, backgroundMusic);
+
+        StartCoroutine(FadeInMusic(audioSource));
+        _oldAudioSource = audioSource;
+    }
+
+    private void IsBackgroundMusicExist(BackgroundMusic backgroundMusic)
+    {
+        if (backgroundMusic == null)
+        {
+            Debug.LogWarning("Background Music: " + name + " not found!");
+            return;
+        }
+    }
+
+    private void IsOldBackgroundMusic(AudioSource oldAudioSource)
+    {
+        if (_oldAudioSource != null)
+        {
+            StartCoroutine(FadeOutMusicAndDestroy(_oldAudioSource));
+        }
+    }
+
+    private void SetBackgroundMusic(AudioSource audioSource, BackgroundMusic backgroundMusic)
+    {
+        audioSource.clip = backgroundMusic.AudioCilp;
+        audioSource.volume = backgroundMusic.Volume;
+        audioSource.pitch = backgroundMusic.Pitch;
+        audioSource.loop = backgroundMusic.Loop;
+    }
     
     private IEnumerator FadeOutMusicAndDestroy(AudioSource oldMusicSource)
     {
         if (oldMusicSource == null)
         {
             yield break;
-        }
-        
+        }     
         float startVolume = oldMusicSource.volume;
         for (float t = 0; t < FadeDuration; t += Time.deltaTime)
         {
             oldMusicSource.volume = Mathf.Lerp(startVolume, 0, t / FadeDuration);
             yield return null;
-        }
+        } // 淡出
         oldMusicSource.Stop();
         Destroy(oldMusicSource); // 銷毀舊音樂物件
     }
@@ -112,7 +125,7 @@ public class AudioManager : MonoBehaviour, IGameService
         {
             newMusic.volume = Mathf.Lerp(0, endVolume, t / FadeDuration);
             yield return null;
-        }
+        } // 淡入
         newMusic.volume = endVolume; // 確保音量設置為最終值
     }
 
